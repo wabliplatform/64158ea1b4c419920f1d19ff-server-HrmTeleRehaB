21c3b8465da91dbd2c90fb7aa595679b52d6495d
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2023-05-03 14:52:38 +0300

    My commit message

--- models/index.js
+++ models/index.js
@@ -1,10 +1,8 @@
 const doctor = require('./doctor');
 const equipment = require('./equipment');
-const p = require('./p');
 const room = require('./room');
 
 module.exports = {
   doctor,
   equipment,
-  p,
   room};
--- services/index.js
+++ services/index.js
@@ -1,11 +1,9 @@
 const DoctorService = require('./DoctorService');
 const EquipmentService = require('./EquipmentService');
-const PService = require('./PService');
 const RoomService = require('./RoomService');
 
 module.exports = {
   DoctorService,
   EquipmentService,
-  PService,
   RoomService,
 };
